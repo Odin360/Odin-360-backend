cf94b2460b79d65da508cb7c3e69e597a697d40c
Your Name <your@email.com>
2025-05-10 03:36:56 +0000

    initialized springboot backend

---
+++ src/main/java/com/Odin360/Odin360Application.java
@@ -0,0 +1,13 @@
+package com.Odin360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Odin360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Odin360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Odin360/Odin360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Odin360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Odin360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
