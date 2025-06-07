6aabec95e79d26a70a845783497f669cb51ea9df
Your Name <your@email.com>
2025-06-07 11:25:24 +0000

    fix mixed repositories

--- src/main/java/com/Odin360/Domains/Dtos/CreateTeamDto.java
+++ src/main/java/com/Odin360/Domains/Dtos/CreateTeamDto.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CreateTeamDto {
     private String name;
-    private String drive;
     private String description;
+    private String drive;
+
 }
